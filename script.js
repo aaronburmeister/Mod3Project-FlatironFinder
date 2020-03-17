c34df65f4172f20eac876de242380fd5a6c961ae
aaronburmeister <f34r91@gmail.com>
2020-03-16 18:30:40 -0600

    Fixed URL again

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000/"
+const BASE_URL = "https://flatiron-finder-backend.herokuapp.com/"
 
 // All main pages
 const splashMain = document.querySelector('.splash-main')
